e295732d00be95c72bca46b164691b2078bfd1dc
Etienne Orliac <etienne.orliac@epfl.ch>
2023-02-02 13:57:30 +0100

    Update on bipp installations on izar and jed

--- install_bipp_on_izar.sh
+++ install_bipp_on_izar.sh
@@ -6,28 +6,52 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 cd $SCRIPT_DIR
 
-BIPP_ROOT=${SCRIPT_DIR}/bipp-izar
+BIPP_NAME=bipp-izar
+
+BIPP_ROOT=${SCRIPT_DIR}/${BIPP_NAME}
 echo BIPP_ROOT = $BIPP_ROOT
 
-
+# Delete + Git clone 
 #[ -d $BIPP_ROOT ] && rm -rf ${BIPP_ROOT}
-#git clone https://github.com/epfl-radio-astro/bipp.git bipp-izar
+#git clone https://github.com/epfl-radio-astro/bipp.git ${BIPP_NAME}
+
 [ -d $BIPP_ROOT ] || (echo "-E- bipp root directory does not exist" && exit 1)
 
+# Activate Spack environment for izar
 source ~/SKA/ska-spack-env/env-bipp-izar/activate.sh
+python -V
 
-# Install bipp from scratch
-# 
+# Just in case, remove bipp from outside any virtual env
+python3 -m pip uninstall -y bipp
+
+# Create a Python virtual environment for izar
+# TODO(eo): check whether that allows to install and run simultaneously from jed and izar
+VENV=VENV_BIPPIZARGCC
+python -m venv $VENV
+source $VENV/bin/activate
+
+# Activate this block to install bipp from scratch
 if [ 1 == 1 ]; then 
+    python3 -m pip uninstall -y bipp
     SKBUILD=${BIPP_ROOT}/_skbuild;              [ -d $SKBUILD ] && rm -r $SKBUILD
     EGGINFO=${BIPP_ROOT}/bipp.egg-info;         [ -d $EGGINFO ] && rm -r $EGGINFO
     EGGINFO=${BIPP_ROOT}/python/bipp.egg-info/; [ -d $EGGINFO ] && rm -r $EGGINFO
-    python3 -m pip uninstall -y bipp
-    #export BIPP_CMAKE_ARGS="-DBLUEBILD_BUILD_TYPE=RelWithDebInfo "
-    #export CUDAFLAGS="-g -G"
-    export BIPP_VC=ON
-    BIPP_GPU=CUDA python3 -m pip install --user --verbose --no-deps --no-build-isolation ${BIPP_ROOT}
 fi
+
+#export BIPP_CMAKE_ARGS="-DBIPP_BUILD_TYPE=Debug"
+#export CUDAFLAGS="-g -G"
+export BIPP_VC=ON
+# Outside a venv
+#BIPP_GPU=CUDA python3 -m pip install --user --verbose --no-deps --no-build-isolation ${BIPP_ROOT} # For prod
+#BIPP_GPU=CUDA python3 -m pip install --user --verbose --no-deps -e ${BIPP_ROOT} # For dev
+# Inside a venv
+BIPP_GPU=CUDA python3 -m pip install --verbose --no-deps --no-build-isolation ${BIPP_ROOT} # For prod
+#BIPP_GPU=CUDA python3 -m pip install --verbose --no-deps -e ${BIPP_ROOT} # For dev
+
+python -c "import bipp"
+echo "-I- test [python -c \"import bipp\"] successful"
+
+deactivate
 
 source ~/SKA/ska-spack-env/env-bipp-izar/deactivate.sh
 
--- install_bipp_on_jed.sh
+++ install_bipp_on_jed.sh
@@ -3,6 +3,7 @@
 set -e
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 cd $SCRIPT_DIR
 
 BIPP_NAME=bipp-jed
@@ -10,22 +11,41 @@ BIPP_NAME=bipp-jed
 BIPP_ROOT=${SCRIPT_DIR}/${BIPP_NAME}
 echo BIPP_ROOT = $BIPP_ROOT
 
-[ -d $BIPP_ROOT ] && rm -rf ${BIPP_ROOT}
-git clone https://github.com/epfl-radio-astro/bipp.git ${BIPP_NAME}
+# Delete + Git clone 
+#[ -d $BIPP_ROOT ] && rm -rf ${BIPP_ROOT}
+#git clone https://github.com/epfl-radio-astro/bipp.git ${BIPP_NAME}
+
 [ -d $BIPP_ROOT ] || (echo "-E- bipp root directory does not exist" && exit 1)
 
+# Activate Spack environment for izar
 source ~/SKA/ska-spack-env/bipp-jed-gcc/activate.sh
+python -V
 
-# Install bipp from scratch
-# 
+# Just in case, remove bipp from outside any virtual env
+python3 -m pip uninstall -y bipp
+
+# Create a Python virtual environment for izar
+# TODO(eo): check whether that allows to install and run simultaneously from jed and izar
+VENV=VENV_BIPPJEDGCC
+python -m venv $VENV
+source $VENV/bin/activate
+
+# Activate this block to install bipp from scratch
 if [ 1 == 1 ]; then 
+    python3 -m pip uninstall -y bipp
     SKBUILD=${BIPP_ROOT}/_skbuild;              [ -d $SKBUILD ] && rm -r $SKBUILD
     EGGINFO=${BIPP_ROOT}/bipp.egg-info;         [ -d $EGGINFO ] && rm -r $EGGINFO
     EGGINFO=${BIPP_ROOT}/python/bipp.egg-info/; [ -d $EGGINFO ] && rm -r $EGGINFO
-    python3 -m pip uninstall -y bipp
-    #export BIPP_CMAKE_ARGS="-DBLUEBILD_BUILD_TYPE=RelWithDebInfo "
-    BIPP_GPU=OFF python3 -m pip install --user --verbose --no-deps --no-build-isolation ${BIPP_ROOT}
 fi
+
+#export BIPP_CMAKE_ARGS="-DBLUEBILD_BUILD_TYPE=RelWithDebInfo "
+#BIPP_GPU=OFF python3 -m pip install --user --verbose --no-deps --no-build-isolation ${BIPP_ROOT}
+BIPP_GPU=OFF python3 -m pip install --verbose --no-deps --no-build-isolation ${BIPP_ROOT}
+
+python -c "import bipp"
+echo "-I- test [python -c \"import bipp\"] successful"
+
+deactivate
 
 source ~/SKA/ska-spack-env/bipp-jed-gcc/deactivate.sh
 
