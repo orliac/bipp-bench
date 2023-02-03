5cfc3fd0a9b4df6af42e31f39b4222ee2290d638
Etienne Orliac <etienne.orliac@epfl.ch>
2023-02-03 09:36:40 +0100

    Updating install scripts for bipp for izar and jed

--- install_bipp_on_jed.sh
+++ install_bipp_on_jed.sh
@@ -6,44 +6,47 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 cd $SCRIPT_DIR
 
-BIPP_NAME=bipp-jed
+CLUSTER=jed
+PACKAGE=bipp
 
-BIPP_ROOT=${SCRIPT_DIR}/${BIPP_NAME}
-echo BIPP_ROOT = $BIPP_ROOT
+PACKAGE_NAME="${PACKAGE}-${CLUSTER}"
+PACKAGE_ROOT=${SCRIPT_DIR}/${PACKAGE_NAME}
+echo PACKAGE_ROOT = ${PACKAGE_ROOT}
 
-# Delete + Git clone 
-#[ -d $BIPP_ROOT ] && rm -rf ${BIPP_ROOT}
-#git clone https://github.com/epfl-radio-astro/bipp.git ${BIPP_NAME}
+# Delete if exists already + Git clone 
+if [ 1 == 0 ]; then
+    [ -d $PACKAGE_ROOT ] && rm -rf ${PACKAGE_ROOT}
+    git clone https://github.com/epfl-radio-astro/${PACKAGE}.git ${PACKAGE_ROOT}
+fi
 
-[ -d $BIPP_ROOT ] || (echo "-E- bipp root directory does not exist" && exit 1)
+[ -d $PACKAGE_ROOT ] || (echo "-E- ${PACKAGE_ROOT} directory does not exist" && exit 1)
 
 # Activate Spack environment for izar
 source ~/SKA/ska-spack-env/bipp-jed-gcc/activate.sh
 python -V
 
 # Just in case, remove bipp from outside any virtual env
-python3 -m pip uninstall -y bipp
+python -m pip uninstall -y ${PACKAGE}
 
-# Create a Python virtual environment for izar
-# TODO(eo): check whether that allows to install and run simultaneously from jed and izar
-VENV=VENV_BIPPJEDGCC
+# Create a Python virtual environment
+VENV=VENV_JEDGCC
 python -m venv $VENV
 source $VENV/bin/activate
 
-# Activate this block to install bipp from scratch
+# Activate this block to install from scratch
 if [ 1 == 1 ]; then 
-    python3 -m pip uninstall -y bipp
-    SKBUILD=${BIPP_ROOT}/_skbuild;              [ -d $SKBUILD ] && rm -r $SKBUILD
-    EGGINFO=${BIPP_ROOT}/bipp.egg-info;         [ -d $EGGINFO ] && rm -r $EGGINFO
-    EGGINFO=${BIPP_ROOT}/python/bipp.egg-info/; [ -d $EGGINFO ] && rm -r $EGGINFO
+    python -m pip uninstall -y ${PACKAGE}
+    SKBUILD=${PACKAGE_ROOT}/_skbuild;                    [ -d $SKBUILD ] && rm -r $SKBUILD
+    EGGINFO=${PACKAGE_ROOT}/${PACKAGE}.egg-info;         [ -d $EGGINFO ] && rm -r $EGGINFO
+    EGGINFO=${PACKAGE_ROOT}/python/${PACKAGE}.egg-info/; [ -d $EGGINFO ] && rm -r $EGGINFO
 fi
 
 #export BIPP_CMAKE_ARGS="-DBLUEBILD_BUILD_TYPE=RelWithDebInfo "
-#BIPP_GPU=OFF python3 -m pip install --user --verbose --no-deps --no-build-isolation ${BIPP_ROOT}
-BIPP_GPU=OFF python3 -m pip install --verbose --no-deps --no-build-isolation ${BIPP_ROOT}
+#BIPP_GPU=OFF python -m pip install --user --verbose --no-deps --no-build-isolation ${PACKAGE_ROOT}
+BIPP_GPU=OFF python -m pip install --verbose --no-deps --no-build-isolation ${PACKAGE_ROOT}
 
-python -c "import bipp"
-echo "-I- test [python -c \"import bipp\"] successful"
+python -c "import ${PACKAGE}"
+echo "-I- test [python -c \"import ${PACKAGE}\"] successful"
 
 deactivate
 
