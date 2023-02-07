095fa6e78024dd981cf9074bafbc309d00f3e6e7
Etienne Orliac <etienne.orliac@epfl.ch>
2023-02-08 00:06:47 +0100

    Added speedup plots + cpu speedup from izar to jed

--- check_solutions.py
+++ check_solutions.py
@@ -52,37 +52,188 @@ for package in list_directories(bench_root):                                    
 #print(ifims['bipp'])
 
 colors = {}
-colors['pypeline'] = {'none': 'black', 'cpu': 'indianred', 'gpu': 'palegreen'}
-colors['bipp']     = {                 'cpu': 'red', 'gpu': 'lawngreen'}
-markers = {'pypeline': '-o', 'bipp': '-*'}
+colors['pypeline'] = {'none': 'black', 'cpu': 'cornflowerblue', 'gpu': 'plum'}
+colors['bipp']     = {                 'cpu': 'blue',           'gpu': 'fuchsia'}
+
+markers = {'pypeline': '--o', 'bipp': '--D'}
 
 # Make plots
-for cluster in 'izar',:
-    for nsta in 15,30,60:
-        for nlev in 1,2,4,16,32:
-            if nlev > nsta:
-                continue
-            plt_name = f"pypeline_izar_nsta{nsta}_nlev{nlev}.png"
-            print(f"-I- Plotting nsta={nsta} nlev={nlev}: {plt_name}")
-            
-            fig, ax = plt.subplots()
-            
-            ax.set(xlabel='Image width & height [pixel]',
-                   ylabel='Time to solution [s]',
-                   title=f"cluster {cluster}, {precision} precision, nsta = {nsta}, nlev = {nlev}")
-            sols =[('pypeline', 'none', 'gcc'),
-                   ('pypeline', 'cpu', 'gcc'), ('pypeline', 'gpu', 'cuda'),
-                   ('bipp', 'cpu', 'gcc'), ('bipp', 'gpu', 'cuda')]
 
-            for package, proc_unit, compiler in sols:
-                ifims_sorted = dict(sorted(ifims[package][cluster][proc_unit][compiler]['double']['ss'][nsta][nlev].items()))
-                x, y = ifims_sorted.keys(), ifims_sorted.values()
-                print(x, "\n", y)
-                ax.plot(x, y, markers[package], color=colors[package][proc_unit], label=f"{package} {proc_unit}")
+### Plot tts jed vs izar
+for nsta in 15,30,60:
+    for nlev in 1,2,4,16,32:
+        if nlev > nsta:
+            continue
+        plt_name = f"jed_vs_izar_nsta{nsta}_nlev{nlev}_tts.png"        
+        fig, ax = plt.subplots()
+        ax.set(xlabel='Image width & height [pixel]',
+               ylabel='Speedup factor from izar to jed [-]',
+               title=f"jed vs izar, {precision} precision, nsta = {nsta}, nlev = {nlev}")
+        sols =[('jed', 'pypeline', 'cpu', 'gcc')]
+        cluster_ref, package_ref, proc_unit_ref, compiler_ref = ('izar', 'pypeline', 'cpu', 'gcc')
+        ref_ifims_sorted = dict(sorted(ifims[package_ref][cluster_ref][proc_unit_ref][compiler_ref]['double']['ss'][nsta][nlev].items()))
+        x_ref, y_ref = ref_ifims_sorted.keys(), ref_ifims_sorted.values()
+        for cluster, package, proc_unit, compiler in sols:
+            ifims_sorted = dict(sorted(ifims[package][cluster][proc_unit][compiler]['double']['ss'][nsta][nlev].items()))
+            x, y = ifims_sorted.keys(), ifims_sorted.values()
+            if x != x_ref:
+                raise Exception("x and x_ref are expected to be the same.")
+            if proc_unit != proc_unit_ref:
+                raise Exception("proc_unit and proc_unit_ref are expected to be the same.")
+            speedups = [tts_ref / tts for tts_ref, tts in zip(y_ref, y)]
+            ax.plot(x, speedups, markers[package], color=colors[package][proc_unit], label=f"{package} {proc_unit}",
+                    linewidth=1)
+            
+        ### bipp with VC=ON on izar, but not on bipp!!
+        sols =[('jed', 'bipp', 'cpu', 'gcc')]
+        cluster_ref, package_ref, proc_unit_ref, compiler_ref = ('izar', 'bipp', 'cpu', 'gcc')
+        ref_ifims_sorted = dict(sorted(ifims[package_ref][cluster_ref][proc_unit_ref][compiler_ref]['double']['ss'][nsta][nlev].items()))
+        x_ref, y_ref = ref_ifims_sorted.keys(), ref_ifims_sorted.values()
+        for cluster, package, proc_unit, compiler in sols:
+            ifims_sorted = dict(sorted(ifims[package][cluster][proc_unit][compiler]['double']['ss'][nsta][nlev].items()))
+            x, y = ifims_sorted.keys(), ifims_sorted.values()
+            if x != x_ref:
+                raise Exception("x and x_ref are expected to be the same.")
+            if proc_unit != proc_unit_ref:
+                raise Exception("proc_unit and proc_unit_ref are expected to be the same.")
+            speedups = [tts_ref / tts for tts_ref, tts in zip(y_ref, y)]
+            ax.plot(x, speedups, markers[package], color=colors[package][proc_unit], label=f"{package} {proc_unit}",
+                    linewidth=1)
+ 
+
+        ax.set_ylim(bottom=0.99)
+
+        """
+        for cluster, package, proc_unit, compiler in sols:
+            ifims_sorted = dict(sorted(ifims[package][cluster][proc_unit][compiler]['double']['ss'][nsta][nlev].items()))
+            x, y = ifims_sorted.keys(), ifims_sorted.values()
+            print(x, "\n", y)
+            ax.plot(x, y, markers[package], color=colors[package][proc_unit], label=f"{cluster} {package} {proc_unit}",
+                    linewidth=1)
+        """ 
+        #ax.set_yscale('log', base=10)
+        ax.set_xscale('log', base=2)
+        ax.legend()
+        fig.savefig(plt_name)
+        plt.show()
+        plt.close()
+
+        sys.exit(0)
+
+
+cluster = 'jed'
+for nsta in 15,30,60:
+    for nlev in 1,2,4,16,32:
+        if nlev > nsta:
+            continue
+
+        plt_name = f"{cluster}_nsta{nsta}_nlev{nlev}_tts.png"        
+
+        fig, ax = plt.subplots()
+
+        ax.set(xlabel='Image width & height [pixel]',
+               ylabel='Time to solution [s]',
+               title=f"cluster {cluster}, {precision} precision, nsta = {nsta}, nlev = {nlev}")
+        sols =[('pypeline', 'none', 'gcc'),
+               ('pypeline', 'cpu', 'gcc'),
+               ('bipp', 'cpu', 'gcc')]
+
+        for package, proc_unit, compiler in sols:
+            ifims_sorted = dict(sorted(ifims[package][cluster][proc_unit][compiler]['double']['ss'][nsta][nlev].items()))
+            x, y = ifims_sorted.keys(), ifims_sorted.values()
+            print(x, "\n", y)
+            ax.plot(x, y, markers[package], color=colors[package][proc_unit], label=f"{package} {proc_unit}",
+                    linewidth=1)
                 
-            ax.set_yscale('log', base=10)
-            ax.set_xscale('log', base=2)
-            ax.legend()
-            fig.savefig(plt_name)
-            plt.show()
+        ax.set_yscale('log', base=10)
+        ax.set_xscale('log', base=2)
+        ax.legend()
+        fig.savefig(plt_name)
+        #plt.show()
+        plt.close()
+
+        plt_name = f"{cluster}_nsta{nsta}_nlev{nlev}_speedup.png"
+        fig, ax = plt.subplots()
+        ax.set(xlabel='Image width & height [pixel]',
+               ylabel='Speedup factor [-]',
+               title=f"cluster {cluster}, {precision} precision, nsta = {nsta}, nlev = {nlev}")
+        sols =[('pypeline', 'cpu', 'gcc'), ('bipp', 'cpu', 'gcc')]
+        package_ref, proc_unit_ref, compiler_ref = ('pypeline', 'none', 'gcc')
+        ref_ifims_sorted = dict(sorted(ifims[package_ref][cluster][proc_unit_ref][compiler_ref]['double']['ss'][nsta][nlev].items()))
+        x_ref, y_ref = ref_ifims_sorted.keys(), ref_ifims_sorted.values()
+        for package, proc_unit, compiler in sols:
+            ifims_sorted = dict(sorted(ifims[package][cluster][proc_unit][compiler]['double']['ss'][nsta][nlev].items()))
+            x, y = ifims_sorted.keys(), ifims_sorted.values()
+            if x != x_ref:
+                raise Exception("x and x_ref are expected to be the same.")
+            speedups = [tts_ref / tts for tts_ref, tts in zip(y_ref, y)]
+            ax.plot(x, speedups, markers[package], color=colors[package][proc_unit], label=f"{package} {proc_unit}",
+                    linewidth=1)
+        ax.set_ylim(bottom=0.99)
+        ax.set_yscale('log', base=10)
+        ax.set_xscale('log', base=2)
+        ax.legend()
+        fig.savefig(plt_name)
+        #plt.show()
+        #sys.exit(0)
+
+cluster = 'izar'
+for nsta in 15,30,60:
+    for nlev in 1,2,4,16,32:
+        if nlev > nsta:
+            continue
+        plt_name = f"{cluster}_nsta{nsta}_nlev{nlev}_tts.png"
+        print(f"-I- Plotting nsta={nsta} nlev={nlev}: {plt_name}")
         
+        fig, ax = plt.subplots()
+            
+        ax.set(xlabel='Image width & height [pixel]',
+               ylabel='Time to solution [s]',
+               title=f"cluster {cluster}, {precision} precision, nsta = {nsta}, nlev = {nlev}")
+
+        sols =[('pypeline', 'none', 'gcc'),
+               ('pypeline', 'cpu', 'gcc'), ('pypeline', 'gpu', 'cuda'),
+               ('bipp', 'cpu', 'gcc'), ('bipp', 'gpu', 'cuda')]
+
+        for package, proc_unit, compiler in sols:
+            ifims_sorted = dict(sorted(ifims[package][cluster][proc_unit][compiler]['double']['ss'][nsta][nlev].items()))
+            x, y = ifims_sorted.keys(), ifims_sorted.values()
+            #print(x, "\n", y)
+            ax.plot(x, y, markers[package], color=colors[package][proc_unit], label=f"{package} {proc_unit}",
+                    linewidth=1)
+                
+        ax.set_yscale('log', base=10)
+        ax.set_xscale('log', base=2)
+        ax.legend()
+        fig.savefig(plt_name)
+        #plt.show()
+        plt.close()
+
+        plt_name = f"{cluster}_nsta{nsta}_nlev{nlev}_speedup.png"
+        fig, ax = plt.subplots()
+        ax.set(xlabel='Image width & height [pixel]',
+               ylabel='Speedup factor [-]',
+               title=f"cluster {cluster}, {precision} precision, nsta = {nsta}, nlev = {nlev}")
+        sols =[('pypeline', 'cpu', 'gcc'), ('pypeline', 'gpu', 'cuda'),
+               ('bipp', 'cpu', 'gcc'), ('bipp', 'gpu', 'cuda')]
+        package_ref, proc_unit_ref, compiler_ref = ('pypeline', 'none', 'gcc')
+        ref_ifims_sorted = dict(sorted(ifims[package_ref][cluster][proc_unit_ref][compiler_ref]['double']['ss'][nsta][nlev].items()))
+        x_ref, y_ref = ref_ifims_sorted.keys(), ref_ifims_sorted.values()
+        for package, proc_unit, compiler in sols:
+            ifims_sorted = dict(sorted(ifims[package][cluster][proc_unit][compiler]['double']['ss'][nsta][nlev].items()))
+            x, y = ifims_sorted.keys(), ifims_sorted.values()
+            if x != x_ref:
+                raise Exception("x and x_ref are expected to be the same.")
+            speedups = [tts_ref / tts for tts_ref, tts in zip(y_ref, y)]
+            ax.plot(x, speedups, markers[package], color=colors[package][proc_unit], label=f"{package} {proc_unit}",
+                    linewidth=1)
+
+        ax.set_ylim(bottom=0.99)
+        ax.set_yscale('log', base=10)
+        ax.set_xscale('log', base=2)
+        ax.legend()
+        fig.savefig(plt_name)
+        #plt.show()
+  
+        #sys.exit(0)
+
