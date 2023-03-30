bc078eb69c8e112b7ecc5fe8dcf446769019342c
Etienne Orliac <etienne.orliac@epfl.ch>
2023-03-30 13:07:01 +0200

    Adding switches for flexibility

--- sbatch_generic.sh
+++ sbatch_generic.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+echo
+env | grep SLURM
+echo
+
 get_option_value() {
     option=$2
     opt_array=($1)
@@ -41,7 +45,12 @@ done
 
 # Note: this file is expected to be copied where benchmark.in to be run resides
 
+echo
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+echo "-I- OMP_NUM_THREADS = $OMP_NUM_THREADS"
+echo
+[ -x "$(command -v nvidia-smi)" ] && nvidia-smi
+echo
 
 input_file=benchmark.in
 [[ -f $input_file ]] || (echo "-E- Input file >>$input_file<< not found." \
@@ -72,62 +81,77 @@ OUT_DIR="$(get_option_value "$input_line" "--output_directory")"
 [ ! -d $OUT_DIR ] && mkdir -pv $OUT_DIR
 cd $OUT_DIR
 
+SLURMOUT_BASENAME=slurm-${SLURM_ARRAY_JOB_ID}_${SLURM_ARRAY_TASK_ID}.out
+ln -s $SOL_DIR/${SLURMOUT_BASENAME} $OUT_DIR/${SLURMOUT_BASENAME}
 
 ## WARNING: CASA must be run before activating the environments!!
 #           (conflits otherwise)
 
+RUN_CASA=0
+RUN_WSCLEAN=1
+
 echo =============================================================================
 echo CASA
 echo =============================================================================
-CASA=~/SKA/casa-6.5.3-28-py3.8/bin/casa
-[ -f $CASA ] || (echo "Fatal. Could not find $CASA" && exit 1)
-which $CASA
-#$CASA --help
-CASA_OUT=dirty_casa
-CASA_LOG=casa.log
-$CASA \
-    --nogui \
-    --norc \
-    --notelemetry \
-    --logfile ${CASA_LOG} \
-    -c $SOL_DIR/casa_tclean.py \
-    --ms_file ${MS_FILE} \
-    --out_name ${CASA_OUT} \
-    --imsize ${WSC_SIZE} \
-    --cell ${WSC_SCALE} \
-    --spw '*:0'
-echo; echo
+if [[ $RUN_CASA == 1 ]]; then
+    CASA=~/SKA/casa-6.5.3-28-py3.8/bin/casa
+    [ -f $CASA ] || (echo "Fatal. Could not find $CASA" && exit 1)
+    which $CASA
+    #$CASA --help
+    CASA_OUT=dirty_casa
+    CASA_LOG=casa.log
+    $CASA \
+        --nogui \
+        --norc \
+        --notelemetry \
+        --logfile ${CASA_LOG} \
+        -c $SOL_DIR/casa_tclean.py \
+        --ms_file ${MS_FILE} \
+        --out_name ${CASA_OUT} \
+        --imsize ${WSC_SIZE} \
+        --cell ${WSC_SCALE} \
+        --spw '*:0'
+    echo; echo
 
-source ${ENV_SPACK}/activate.sh
-source ${VENV}/bin/activate
+    source ${ENV_SPACK}/activate.sh
+    source ${VENV}/bin/activate
 
-echo
-python $SOL_DIR/casa_log_to_json.py --casa_log ${CASA_LOG}
-echo
+    echo
+    python $SOL_DIR/casa_log_to_json.py --casa_log ${CASA_LOG}
+    echo
+else
+    source ${ENV_SPACK}/activate.sh
+    source ${VENV}/bin/activate
+fi
 
 echo
 echo =============================================================================
 echo WSClean
 echo =============================================================================
 echo
-WSCLEAN_OUT=dirty_wsclean
-WSCLEAN_LOG=wsclean.log
-time wsclean \
-    -verbose \
-    -log-time \
-    -channel-range 0 1 \
-    -size ${WSC_SIZE} ${WSC_SIZE} \
-    -scale ${WSC_SCALE}asec \
-    -pol I \
-    -weight natural \
-    -niter 0 \
-    -name ${WSCLEAN_OUT} \
-    ${MS_FILE} \
-    | tee ${WSCLEAN_LOG}
-
-echo
-python $SOL_DIR/wsclean_log_to_json.py --wsc_log ${WSCLEAN_LOG}
-echo
+#-even-timesteps \
+if [[ $RUN_WSCLEAN == 1 ]]; then
+    WSCLEAN_OUT=dirty_wsclean
+    WSCLEAN_LOG=wsclean.log
+    time wsclean \
+        -verbose \
+        -log-time \
+        -channel-range 0 1 \
+        -size ${WSC_SIZE} ${WSC_SIZE} \
+        -scale ${WSC_SCALE}asec \
+        -pol I \
+        -weight natural \
+        -niter 0 \
+        -name ${WSCLEAN_OUT} \
+        -interval 0 1 \
+        -make-psf \
+        ${MS_FILE} \
+        | tee ${WSCLEAN_LOG}
+    
+    echo
+    python $SOL_DIR/wsclean_log_to_json.py --wsc_log ${WSCLEAN_LOG}
+    echo
+fi
 
 cd -
 
@@ -135,7 +159,22 @@ echo ===========================================================================
 echo ${pipeline}
 echo =============================================================================
 echo
-time python ${pipeline}_${algo}_${package}.py $input_line
+BLUEBILD_LOG=$OUT_DIR/bluebild.log
+BLUEBILD_TIME_LOG=$OUT_DIR/bluebild_time.log
+
+export CUDA_ENABLE_COREDUMP_ON_EXCEPTION=1
+
+time -p (python -u ${pipeline}_${algo}_${package}.py $input_line | tee ${BLUEBILD_LOG}) 2> ${BLUEBILD_TIME_LOG}
+#time -p (cuda-gdb --args python -u ${pipeline}_${algo}_${package}.py $input_line | tee ${BLUEBILD_LOG}) 2> ${BLUEBILD_TIME_LOG}
+
+echo
+echo "cat ${BLUEBILD_TIME_LOG}"
+echo "=========================================================================="
+cat ${BLUEBILD_TIME_LOG}
+echo "=========================================================================="
+
+echo
+python $SOL_DIR/add_time_stats_to_bluebild_json.py --bb_json $OUT_DIR/stats.json --bb_time_log ${BLUEBILD_TIME_LOG}
 echo
 
 echo =============================================================================
@@ -143,14 +182,21 @@ echo Generate plots
 echo =============================================================================
 echo
 cd $OUT_DIR
-python $SOL_DIR/plots.py \
-    --bb_grid   I_lsq_eq_grid.npy \
-    --bb_data   I_lsq_eq_data.npy \
-    --bb_json   stats.json \
-    --wsc_fits  ${WSCLEAN_OUT}-dirty.fits \
-    --wsc_log   ${WSCLEAN_LOG} \
-    --casa_fits ${CASA_OUT}.image.fits \
-    --casa_log  ${CASA_LOG}
+
+PLOTS_CMD=""
+PLOTS_CMD+="python $SOL_DIR/plots.py"
+PLOTS_CMD+=" --bb_grid   I_lsq_eq_grid.npy"
+PLOTS_CMD+=" --bb_data   I_lsq_eq_data.npy"
+PLOTS_CMD+=" --bb_json   stats.json"
+if [[ $RUN_WSCLEAN == 1 ]]; then
+    PLOTS_CMD+=" --wsc_fits  ${WSCLEAN_OUT}-dirty.fits"
+    PLOTS_CMD+=" --wsc_log   ${WSCLEAN_LOG}"
+fi
+if [[ $RUN_CASA == 1 ]]; then
+    PLOTS_CMD+=" --casa_fits ${CASA_OUT}.image.fits"
+    PLOTS_CMD+=" --casa_log  ${CASA_LOG}"
+fi
+$PLOTS_CMD
 
 cd -
 
