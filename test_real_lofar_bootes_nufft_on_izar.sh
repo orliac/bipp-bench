6f99e33f199bf99c731a886de6da9576a8faca9a
Etienne Orliac <etienne.orliac@epfl.ch>
2023-05-03 08:18:42 +0200

    Adding iterating loop (to test bugfix)

--- test_real_lofar_bootes_nufft_on_izar.sh
+++ test_real_lofar_bootes_nufft_on_izar.sh
@@ -15,6 +15,7 @@ source ~/SKA/ska-spack-env/${SPACK_SKA_ENV}/activate.sh
 
 source $VENV/bin/activate
 
+sh ./install_bipp_on_izar.sh orliac
 
 MS_FILE=gauss4_t201806301100_SBL180.MS
 
@@ -40,21 +41,31 @@ if [ 1 == 0 ]; then
 fi
 
 if [ 1 == 1 ]; then
-    #for proc_unit in 'cpu' 'gpu'; do
-    for proc_unit in 'gpu'; do
-        echo "=================================================================================="
-        echo  python real_lofar_bootes_nufft_bipp.py $proc_unit
-        echo "=================================================================================="
-        time python real_lofar_bootes_nufft_bipp.py \
-            --ms_file ${MS_FILE} \
-            --outdir . \
-            --cluster izar \
-            --processing_unit $proc_unit --compiler gcc \
-            --precision double \
-            --package bipp --nlev 4 \
-            --pixw ${WSC_SIZE} --wsc_scale ${WSC_SCALE} \
-            --sigma 0.9999 --time_slice_pe 1 --time_slice_im 1 \
-            --wsc_log ${MS_FILE}_wsclean.log
+
+    for iter in {1..200}; do
+        
+        echo; echo
+        echo ">>>>>>>>>>>>>>>>>> iter $iter"
+        echo
+        #for proc_unit in 'cpu' 'gpu'; do
+        for proc_unit in 'gpu'; do
+            echo "=================================================================================="
+            echo  python real_lofar_bootes_nufft_bipp.py $proc_unit
+            echo "=================================================================================="
+            time \
+                #cuda-memcheck --leak-check full --log-file leak.log \
+            #cuda-gdb --args \
+            python real_lofar_bootes_nufft_bipp.py \
+                --ms_file ${MS_FILE} \
+                --output_directory /scratch/izar/orliac/test_nufft \
+                --cluster izar \
+                --processing_unit $proc_unit --compiler gcc \
+                --precision double \
+                --package bipp --nlev 4 \
+                --pixw ${WSC_SIZE} --wsc_scale ${WSC_SCALE} \
+                --sigma 0.9999 --time_slice_pe 100 --time_slice_im 1 \
+                --wsc_log ${MS_FILE}_wsclean.log
+        done
     done
 fi
 
