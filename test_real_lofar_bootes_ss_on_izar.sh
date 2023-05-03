f53f9a30b0309156fb1a6288016b1e911a1e6649
Etienne Orliac <etienne.orliac@epfl.ch>
2023-05-03 08:16:12 +0200

    Adding WSClean run + plotting section

--- test_real_lofar_bootes_ss_on_izar.sh
+++ test_real_lofar_bootes_ss_on_izar.sh
@@ -24,30 +24,72 @@ source $VENV/bin/activate
 python -m pip list | grep pypeline
 
 
-MS_FILE=gauss4_t201806301100_SBL180.MS
+MS_BASENAME=gauss4_t201806301100_SBL180.MS
+OUT_DIR=$(pwd)/bb_dirty_maps/gauss4/full_fix  ## adapt here!
+IN_DIR=$(pwd)
+MS_FILE=${IN_DIR}/${MS_BASENAME}
+[ -d $MS_FILE ] || (echo "-E- MS dataset $MS_FILE not found" && exit 1)
 
-WSC_SIZE=256 #1000
+WSC_SIZE=1024
 WSC_SCALE=10
-TIME_SLICE_PE=100
-TIME_SLICE_IM=100
+TIME_SLICE_PE=1000
+TIME_SLICE_IM=1000
 SIGMA=0.9999
+
+RUN_WSCLEAN=1
+
+if [[ $RUN_WSCLEAN == 1 ]]; then
+
+    # dirty
+    WSCLEAN_OUT=${OUT_DIR}/${MS_BASENAME}-dirty_wsclean
+    WSCLEAN_LOG=${OUT_DIR}/${MS_BASENAME}-dirty_wsclean.log
+    time wsclean \
+        -verbose \
+        -log-time \
+        -channel-range 0 1 \
+        -size ${WSC_SIZE} ${WSC_SIZE} \
+        -scale ${WSC_SCALE}asec \
+        -pol I \
+        -weight natural \
+        -niter 0 \
+        -make-psf \
+        -interval 0 1 \
+        -name ${WSCLEAN_OUT} \
+        ${MS_FILE} \
+        | tee ${WSCLEAN_LOG}
+    
+    echo
+    python $IN_DIR/wsclean_log_to_json.py --wsc_log ${WSCLEAN_LOG}
+    echo
+fi
 
 if [ 1 == 1 ]; then
     #for proc_unit in 'none' 'cpu' 'gpu'; do
-    for proc_unit in 'cpu' 'gpu'; do
+    for proc_unit in 'none'; do
         echo "=================================================================================="
         echo  python real_lofar_bootes_ss_pypeline.py $proc_unit
         echo "=================================================================================="
         time python real_lofar_bootes_ss_pypeline.py \
             --ms_file ${MS_FILE} \
-            --outdir . \
+            --output_directory ${OUT_DIR} \
             --cluster izar \
             --processing_unit $proc_unit --compiler gcc \
             --precision double \
-            --package pypeline --nlev 1 \
+            --package pypeline --nlev 4 \
             --pixw ${WSC_SIZE} --wsc_scale ${WSC_SCALE} \
             --sigma=${SIGMA} --time_slice_pe ${TIME_SLICE_PE} --time_slice_im ${TIME_SLICE_IM} \
-            --wsc_log ${MS_FILE}_wsclean.log
+            --wsc_log ${WSCLEAN_LOG}
+
+        python plots.py \
+            --bb_grid  ${OUT_DIR}/I_lsq_eq_grid.npy \
+            --bb_data  ${OUT_DIR}/I_lsq_eq_data.npy \
+            --bb_json  ${OUT_DIR}/stats.json \
+            --wsc_log  ${WSCLEAN_LOG} \
+            --wsc_fits ${WSCLEAN_OUT}-dirty.fits \
+            --flip_lr \
+            --outdir   ${OUT_DIR} \
+            --outname  'gauss4'
+
     done
 fi
 
