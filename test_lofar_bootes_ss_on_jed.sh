34a75712f937022318eff2ab53f54c8b085401d9
Etienne Orliac <etienne.orliac@epfl.ch>
2023-05-03 08:20:56 +0200

    Using SLURM number of CPUs if set

--- test_lofar_bootes_ss_on_jed.sh
+++ test_lofar_bootes_ss_on_jed.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 set -e
-
-export OMP_NUM_THREADS=36
+if [[ -z "${OMP_NUM_THREADS}" ]]; then
+    export OMP_NUM_THREADS="${SLURM_CPUS_PER_TASK:-1}"
+fi
+export NUMEXPR_MAX_THREADS=$OMP_NUM_THREADS
 export OPEN_BLAS_NUM_THREADS=1
 
 SPACK_SKA_ENV=bipp-jed-gcc
@@ -11,14 +13,14 @@ VENV=VENV_JEDGCC
 source ~/SKA/ska-spack-env/${SPACK_SKA_ENV}/activate.sh
 source $VENV/bin/activate
 
-python lofar_bootes_ss_bipp.py --outdir . --cluster jed --processing_unit cpu --compiler gcc --precision double --package bipp --nsta 60 --nlev 60 --pixw 256
+python lofar_bootes_ss_bipp.py     --outdir . --cluster jed --processing_unit cpu --compiler gcc --precision double --package bipp --nsta 60 --nlev 60 --pixw 256
 
 echo
 echo "=================================================================================="
 echo "=================================================================================="
 echo
 
-python lofar_bootes_ss_pypeline.py --outdir . --cluster jed --processing_unit cpu --compiler gcc --precision double --package pypeline --nsta 60 --nlev 60 --pixw 256
+#python lofar_bootes_ss_pypeline.py --outdir . --cluster jed --processing_unit cpu --compiler gcc --precision double --package pypeline --nsta 60 --nlev 60 --pixw 256
 
 deactivate
 source ~/SKA/ska-spack-env/${SPACK_SKA_ENV}/deactivate.sh
