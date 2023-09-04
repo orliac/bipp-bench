5d8b41761b8572d4a0ae184752007b02c3609e39
Etienne Orliac <etienne.orliac@epfl.ch>
2023-09-04 11:20:34 +0200

    New options used for generating inputs for benchmark

--- generate_benchmark_input_file.py
+++ generate_benchmark_input_file.py
@@ -21,11 +21,13 @@ parser.add_argument("--time_end_idx",   help="End time index", required=True)
 parser.add_argument("--time_slice_pe",  help="Time slice in parameter estimation", required=False, default=1)
 parser.add_argument("--time_slice_im",  help="Time slice in imaging", required=False, default=1)
 parser.add_argument("--sigma",          help="Fraction of power spectrum to be considered", required=False)
-parser.add_argument("--wsc_scale",      help="WSClean pixel scale in arcsec", required=False)
+#parser.add_argument("--wsc_scale",      help="WSClean pixel scale in arcsec", required=False)
+parser.add_argument("--fov_deg",        help="Field of view in degree", required=True)
 parser.add_argument("--algo",           help="Algorithm to use (ss, nufft)", required=True)
 parser.add_argument("--telescope",      help="Telescope", required=True)
 parser.add_argument("--filter_negative_eigenvalues", help="Filter negative eigenvalues? 0/1", required=True, choices=["0", "1"])
-
+parser.add_argument("--channel_id",     help="Channel ID to process", required=True, type=int)
+parser.add_argument("--outname",        help="Prefix of output files", required=True, type=str)
 
 args = parser.parse_args()
 
@@ -36,29 +38,38 @@ if args.package == "bipp" and args.proc_unit == "none":
 
 # Set up jobs to run in the benchmark
 nLevels   = [1, 2, 4, 8]
-pixWidths = [256, 512, 1024, 2048]
-#nStations = [15, 30, 60]
+pixWidths = [256, 512] #[1024] #[2048]
 nStations = [0] # 0 == all stations
+
+import subprocess
 
 with open(args.in_file, 'w') as f:
     for nsta in np.sort(nStations):
         for nlev in np.sort(nLevels):
             cli_  = f"--cluster {args.cluster} --processing_unit {args.proc_unit} --compiler {args.compiler} "
-            cli_ += f" --precision {args.precision} --package {args.package} --nlev {nlev}  --wsc_scale {args.wsc_scale}"
+            cli_ += f" --precision {args.precision} --package {args.package} --nlev {nlev}  --fov_deg {args.fov_deg} "
+            #--wsc_scale {args.wsc_scale}"
             cli_ += f" --ms_file {args.ms_file}"
             cli_ += f" --time_start_idx {args.time_start_idx} --time_end_idx {args.time_end_idx}"
             cli_ += f" --time_slice_pe {args.time_slice_pe} --time_slice_im {args.time_slice_im}"
             cli_ += f" --sigma {args.sigma} --algo {args.algo} --telescope {args.telescope}"
+            cli_ += f" --channel_id {args.channel_id} --outname {args.outname} --nufft_eps 0.00001"
             cli_ += f" --filter_negative_eigenvalues {args.filter_negative_eigenvalues} "   # Do not forget space at the end!
             
             if nsta == 0: # case all stations
                 for pixw in np.sort(pixWidths):
+                    cmd = ["python3", "get_scale.py", "--pixw", str(pixw), "--fov", args.fov_deg, "--unit", "deg"]
+                    p = subprocess.run(cmd, capture_output=True)
+                    wsc_scale = p.stdout.decode("utf-8").strip()
                     outdir = os.path.join(args.out_dir, str(nsta), str(nlev), str(pixw))
-                    cli = cli_ + f"--nsta 0 --pixw {pixw} --output_directory {outdir}\n"
+                    cli = cli_ + f"--nsta 0 --pixw {pixw} --wsc_scale {wsc_scale} --output_directory {outdir}\n"
                     f.write(cli)
             else:
                 if nlev <= nsta:
                     for pixw in np.sort(pixWidths):
+                        cmd = ["python3", "get_scale.py", "--pixw", str(pixw), "--fov", args.fov_deg, "--unit", "deg"]
+                        p = subprocess.run(cmd, capture_output=True)
+                        wsc_scale = p.stdout.decode("utf-8").strip()
                         outdir = os.path.join(args.out_dir, str(nsta), str(nlev), str(pixw))
-                        cli = cli_ + f"--nsta {nsta} --pixw {pixw} --output_directory {outdir}\n"
+                        cli = cli_ + f"--nsta {nsta} --pixw {pixw} --wsc_scale {wsc_scale} --output_directory {outdir}\n"
                         f.write(cli)
