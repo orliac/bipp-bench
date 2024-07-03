2390bdba671edfe07eef574183e869a0a538b25d
Etienne Orliac <etienne.orliac@epfl.ch>
2024-07-03 07:38:55 +0200

    Adding gridder and wprojplanes in casa_tclean.py

--- casa_tclean.py
+++ casa_tclean.py
@@ -11,6 +11,9 @@ parser.add_argument('--cell',     help='Cell size in asec', type=float, required
 parser.add_argument('--spw',      help='Select spectral window/channels', required=False)
 parser.add_argument('--observation', help='Observation ID range', required=False)
 parser.add_argument('--timerange', help='Range of time to select from data', required=False)
+parser.add_argument('--gridder',  help='CASA gridder to use', choices=['wproject'], required=False)
+parser.add_argument('--wprojplanes', help="CASA number of W projection planes", type=int, required=False)
+
 args = parser.parse_args()
 
 print("-D- casa_tclean.py args =", args)
@@ -24,7 +27,7 @@ print("-D- args.timerange =", args.timerange)
 
 ts = time.time()
 tclean(vis=args.ms_file, imagename=args.out_name, imsize=args.imsize, cell=str(args.cell)+'arcsec', niter=0,
-       selectdata=True, spw=args.spw, timerange=args.timerange)
+       selectdata=True, spw=args.spw, timerange=args.timerange, gridder=args.gridder, wprojplanes=args.wprojplanes)
 te = time.time()
 print(f"#@# dirty_casa {te - ts:.3f}")
 
